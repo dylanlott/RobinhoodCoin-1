7e4523716a61595cd3ca5d7ffc3a20406c89820e
Dylan Lott <lott.dylan@gmail.com>
2018-02-16 22:17:39 -0700

    Test harness for RobinHoodCoin

---
+++ test/RobinHoodCoin.js
@@ -0,0 +1,63 @@
+contract("RobinHoodCoin", (accounts) => {
+  console.log(accounts)
+  const owner = accounts[0]
+
+  console.log('owner is', owner)
+
+  describe('king', () => {
+
+    it('should declare a king if owns 51% of tokens', () => {
+
+    })
+
+  })
+
+  describe('richDudes', () => {
+
+    it('should be able to be stolen from', () => {
+
+    })
+
+  })
+
+  describe('Elites', () => {
+
+    it('should put address into elites', () => {
+
+    })
+
+  })
+
+  describe('Tax', () => {
+
+    it('should go to the king if king exists', () => {
+
+    })
+
+    it('should go to government if no king', () => {
+
+    })
+
+  })
+
+  describe('Payday', () => {
+
+    it('should pay worker out of government address', () => {
+
+    })
+
+  })
+
+  describe('Robbery', () => {
+
+    it('should steal from richDude', () => {
+
+    })
+
+    it('should not allow stealing from poorDude', () => {
+
+    })
+
+  })
+
+})
